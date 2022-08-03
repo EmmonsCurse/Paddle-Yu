455e38fe4c24f0700d1d4256f2fd24822912580d
YuBaoku <49938469+EmmonsCurse@users.noreply.github.com>
2022-08-03 18:57:25 +0800

    Update run_demo_ce.sh

--- run_demo_ce.sh
+++ run_demo_ce.sh
@@ -4,310 +4,53 @@ set -e
 pwd
 
 git clone https://github.com/PaddlePaddle/Paddle-Inference-Demo.git --depth=1
-cd Paddle-Inference-Demo/python/cpu/resnet50
-
-# demo 1: cpu-resnet50 单输入模型 oneDNN/OnnxRuntime 预测样例
-if [ ! -d resnet50 ]; then
-    wget https://paddle-inference-dist.bj.bcebos.com/Paddle-Inference-Demo/resnet50.tgz
-    tar xzf resnet50.tgz 
-fi
-
-if [ ! -f ILSVRC2012_val_00000247.jpeg ]; then
-    wget https://paddle-inference-dist.bj.bcebos.com/inference_demo/python/resnet50/ILSVRC2012_val_00000247.jpeg
-fi
-
-# 使用 oneDNN 运行样例
-python infer_resnet.py --model_file=./resnet50/inference.pdmodel --params_file=./resnet50/inference.pdiparams
-
-# 使用 OnnxRuntime 预测样例
-python infer_resnet.py --model_file=./resnet50/inference.pdmodel --params_file=./resnet50/inference.pdiparams --use_onnxruntime=1
-
-# demo 2: cpu-yolov3 多输入模型 oneDNN/OnnxRuntime 预测样例
-cd ../yolov3
-
-if [ ! -d yolov3_r50vd_dcn_270e_coco ]; then
-    wget https://paddle-inference-dist.bj.bcebos.com/Paddle-Inference-Demo/yolov3_r50vd_dcn_270e_coco.tgz
-    tar xzf yolov3_r50vd_dcn_270e_coco.tgz
-fi
-
-if [ ! -f kite.jpg ]; then
-    wget https://paddle-inference-dist.bj.bcebos.com/inference_demo/images/kite.jpg
-fi
-
-# 使用 oneDNN 运行样例
-python infer_yolov3.py --model_file=./yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file=./yolov3_r50vd_dcn_270e_coco/model.pdiparams
-
-# 使用 OnnxRuntime 预测样例
-# python infer_yolov3.py --model_file=./yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file=./yolov3_r50vd_dcn_270e_coco/model.pdiparams --use_onnxruntime=1
-
-# demo 3: advanced-share_external_data 运行 share_external_data 运行案例
-cd ../../advanced/share_external_data/
-
-if [ ! -f ILSVRC2012_val_00000247.jpeg ]; then
-    wget https://paddle-inference-dist.bj.bcebos.com/inference_demo/python/resnet50/ILSVRC2012_val_00000247.jpeg
-fi
-
-python infer_share_external_data.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams
-
-# demo 4: advanced-multi_thread: 运行多线程运行样例
-cd ../multi_thread/
-
-if [ ! -f ILSVRC2012_val_00000247.jpeg ]; then
-    wget https://paddle-inference-dist.bj.bcebos.com/inference_demo/python/resnet50/ILSVRC2012_val_00000247.jpeg
-fi
-
-python threads_demo.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams --thread_num=2
-
-# demo 5: advanced-custom_operator 自定义算子执行
-cd ../custom_operator/
-python train_and_infer.py
-
-# demo 6: gpu-resnet50 单输入模型 原生gpu/gpu混合精度推理/Trt_fp32/Trt_fp16/Trt_int8/Trt_dynamic_shape 预测样例
-
-cd ../../gpu/resnet50/
-
-if [ ! -f ILSVRC2012_val_00000247.jpeg ]; then
-    wget https://paddle-inference-dist.bj.bcebos.com/inference_demo/python/resnet50/ILSVRC2012_val_00000247.jpeg
-fi
-
-# 使用原生 GPU 运行样例
-python infer_resnet.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams
-
-# 使用GPU 混合精度推理 运行样例
-python infer_resnet.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams --run_mode=gpu_fp16
-
-# 使用 Trt Fp32 运行样例
-python infer_resnet.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams --run_mode=trt_fp32
-
-# 使用 Trt Fp16 运行样例
-python infer_resnet.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams --run_mode=trt_fp16
-
-# 使用 Trt Int8 运行样例
-python infer_resnet.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams --run_mode=trt_int8
-
-python infer_resnet.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams --run_mode=trt_int8
-
-# 使用 Try dynamic shape 运行样例
-# python infer_resnet.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams --run_mode=trt_fp32 --use_dynamic_shape=1
-
-# demo 7: gpu-yolov3 多输入模型 原生gpu/gpu混合精度推理/Trt_fp32/Trt_fp16/Trt_int8/Trt_dynamic_shape 预测样例
-cd ../yolov3/
-
-# download data
-if [ ! -f kite.jpg ]; then
-    wget https://paddle-inference-dist.bj.bcebos.com/inference_demo/images/kite.jpg
-fi
-
-# 使用原生 GPU 运行样例
-python infer_yolov3.py --model_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdiparams
-
-# 使用 Trt Fp32 运行样例
-python infer_yolov3.py --model_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_fp32
-
-# 使用 Trt Fp16 运行样例
-python infer_yolov3.py --model_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_fp16
-
-# 使用 Trt Int8 运行样例
-python infer_yolov3.py --model_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_int8
-
-python infer_yolov3.py --model_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_int8
-
-# demo 8: gpu-tuned_dynamic_shape Trt动态shape自动推导 预测样例 使用 Paddle-TRT TunedDynamicShape 能力
-cd ../tuned_dynamic_shape/
-
-# 首先需要针对业务数据进行离线 tune，来获取计算图中所有中间 tensor 的 shape 范围，并将其存储在 config 中配置的shape_range_info.pbtxt 文件中
-
-python infer_tune.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams --tune 1
-
-# 有了离线 tune 得到的 shape 范围信息后，您可以使用该文件自动对所有的 trt 子图设置其输入的 shape 范围。
-python infer_tune.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams --use_gpu=1 --use_trt 1 --tuned_dynamic_shape 1
-
-# demo 9: 基于ELMo的LAC分词预测样例
-cd ../../mixed/ELMo/
-
-if [ ! -d elmo ]; then
-    wget https://paddle-inference-dist.bj.bcebos.com/Paddle-Inference-Demo/elmo.tgz
-    tar xzf elmo.tgz 
-fi
-
-if [ ! -d elmo_data ]; then
-    wget https://paddle-inference-dist.bj.bcebos.com/inference_demo/python/elmo/elmo_data.tgz
-    tar xzf elmo_data.tgz
-fi
-
-python infer.py
-
-# demo 10: 口罩检测预测样例
-cd ../mask_detection/
-
-# demo 11: LSTM INT8 prediction example on X86 Linux
-cd ../x86_lstm_demo/
-
-# 1. download model FP32 model for post-training quantization
-if [ ! -d lstm_fp32_model ]; then
-    wget https://paddle-inference-dist.bj.bcebos.com/lstm/lstm_fp32_model.tar.gz
-    tar xzf lstm_fp32_model.tar.gz
-fi
-
-# 2. download model quant-aware model
-if [ ! -d lstm_quant ]; then
-    wget https://paddle-inference-dist.bj.bcebos.com/lstm/lstm_quant.tar.gz
-    tar xzf lstm_quant.tar.gz
-fi
-
-# 3. download data
-if [ ! -f quant_lstm_input_data ]; then
-    wget https://paddle-inference-dist.bj.bcebos.com/int8/unittest_model_data/quant_lstm_input_data.tar.gz
-    tar xzf quant_lstm_input_data.tar.gz
-fi
-
-# 4. download save_quant_model file
-if [ ! -f save_quant_model.py ]; then
-    wget https://raw.githubusercontent.com/PaddlePaddle/Paddle/develop/python/paddle/fluid/contrib/slim/tests/save_quant_model.py
-fi
-
-# 1 thread
-# run fp32 model
-python model_test.py --model_path=./lstm_fp32_model --data_path=./quant_lstm_input_data --use_analysis=False --num_threads=1
-
-# run ptq int8 model
-python model_test.py --model_path=./lstm_fp32_model --data_path=./quant_lstm_input_data --use_ptq=True --num_threads=1
-
-# save quant2 int8 model
-python save_quant_model.py --quant_model_path=./lstm_quant --int8_model_save_path=./quant_saved_model_int8
-
-# run quant2 int8 model
-python model_test.py --model_path=./quant_saved_model_int8 --data_path=./quant_lstm_input_data --use_analysis=True --num_threads=1
-
-# 4 thread
-# run fp32 model
-python model_test.py --model_path=./lstm_fp32_model --data_path=./quant_lstm_input_data --use_analysis=False --num_threads=4
-
-# run ptq int8 model
-python model_test.py --model_path=./lstm_fp32_model --data_path=./quant_lstm_input_data --use_ptq=True --num_threads=4
-
-# save quant2 int8 model
-python save_quant_model.py --quant_model_path=./lstm_quant --int8_model_save_path=./quant_saved_model_int8
-
-# run quant2 int8 model
-python model_test.py --model_path=./quant_saved_model_int8 --data_path=./quant_lstm_input_data --use_analysis=True --num_threads=4
-
 
 # c++ 部分
 # 下载预测库
-cd ../../../c++/lib/
+cd Paddle-Inference-Demo/c++/lib/
 
-wget https://paddle-inference-lib.bj.bcebos.com/2.3.1/cxx_c/Linux/GPU/x86-64_gcc8.2_avx_mkl_cuda11.2_cudnn8.2.1_trt8.0.3.4/paddle_inference.tgz
+wget -q https://paddle-inference-lib.bj.bcebos.com/2.3.1/cxx_c/Linux/GPU/x86-64_gcc8.2_avx_mkl_cuda11.2_cudnn8.2.1_trt8.0.3.4/paddle_inference.tgz
 tar xzf paddle_inference.tgz
-
-# demo 1 cpu-resnet50
-cd ../cpu/resnet50/
-
-sed -i "s/WITH_ONNXRUNTIME=ON/WITH_ONNXRUNTIME=OFF/" compile.sh
-
-# 使用 oneDNN 运行样例
-sh run.sh
-
-# 使用 OnnxRuntime 运行样例
-./build/resnet50_test --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --use_ort=1
-
-
-# demo 2 cpu-yolov3
-# 使用 oneDNN 运行样例
-cd ../yolov3/ && sh run.sh
-
-# 使用 OnnxRuntime 运行样例 -error
-# ./build/yolov3_test --model_file yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file yolov3_r50vd_dcn_270e_coco/model.pdiparams --use_ort=1
-
-
-# demo 3 gpu-resnet50
-# 使用原生 GPU 运行样例
-cd ../../gpu/resnet50/
-sed -i "s/TensorRT-7.1.3.4/TensorRT-8.0.3.4/" compile.sh && sh run.sh
-
-# 使用 TensorRT Fp32 运行样例
-./build/resnet50_test --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --run_mode=trt_fp32
-
-# 使用 TensorRT Fp16 运行样例
-./build/resnet50_test --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --run_mode=trt_fp16
-
-
-# 使用 TensorRT Int8 离线量化预测运行样例
-# 生成量化校准表
-./build/resnet50_test --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --run_mode=trt_int8
-
-# 加载校准表执行预测
-./build/resnet50_test --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --run_mode=trt_int8 --use_calib=true
-
-# 使用 TensorRT 加载 PaddleSlim Int8 量化模型预测
-# 模型下载 
-wget https://paddle-inference-dist.bj.bcebos.com/inference_demo/python/resnet50/ResNet50_quant.tar.gz
-tar -zxvf ResNet50_quant.tar.gz
-
-./build/resnet50_test --model_dir ResNet50_quant/ --run_mode=trt_int8 --use_calib=false
-
-# 使用 TensorRT dynamic shape 运行样例（以 Fp32 为例）-error
-# ./build/resnet50_test --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --run_mode=trt_fp32 --use_dynamic_shape=1
-
-
-# demo 4 gpu-yolov3
-# 使用原生 GPU 运行样例
-cd ../yolov3/ && sed -i "s/TensorRT-7.1.3.4/TensorRT-8.0.3.4/" compile.sh && sh run.sh
-
-# 使用 TensorRT Fp32 运行样例
-./build/yolov3_test --model_file yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_fp32
-
-# 使用 TensorRT Fp16 运行样例
-./build/yolov3_test --model_file yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_fp16
-
-# 使用 TensorRT Int8 运行样例
-# 生成量化校准表
-./build/yolov3_test --model_file yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_int8
-
-# 加载校准表执行预测
-./build/yolov3_test --model_file yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_int8
-
-
-# 使用 TensorRT dynamic shape 运行样例（以 Fp32 为例）-error
-# ./build/yolov3_test --model_file yolov3_r50vd_dcn_270e_coco/inference.pdmodel --params_file yolov3_r50vd_dcn_270e_coco/inference.pdiparams --run_mode=trt_fp32 --use_dynamic_shape=1
 
 
 # demo 5 gpu-tuned_dynamic_shape
-cd ../tuned_dynamic_shape/ && sed -i "s/TensorRT-7.1.3.4/TensorRT-8.0.3.4/" compile.sh
+cd ../gpu/tuned_dynamic_shape/ && sed -i "s/TensorRT-7.1.3.4/TensorRT-8.0.3.4/" compile.sh
 
 # 获取 PaddleClas 模型
 git clone https://github.com/PaddlePaddle/PaddleClas.git --depth=1
-cd PaddleClas && pwd
+cd PaddleClas
+
+python -m pip install -r requirements.txt -i https://mirror.baidu.com/pypi/simple --trusted-host mirror.b+aidu.com
 
 mkdir pretrained
 mkdir inference_model
 
 # ResNet50_vd
-wget --no-proxy -P pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/legendary_models/ResNet50_vd_pretrained.pdparams
+wget --no-proxy -P -q pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/legendary_models/ResNet50_vd_pretrained.pdparams
 python tools/export_model.py -c ppcls/configs/ImageNet/ResNet/ResNet50_vd.yaml -o Global.pretrained_model=./pretrained/ResNet50_vd_pretrained -o Global.save_inference_dir=./inference_model/ResNet50_vd
 
 # GhostNet_x1_0
-wget --no-proxy -P pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/GhostNet_x1_0_pretrained.pdparams
+wget --no-proxy -P -q pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/GhostNet_x1_0_pretrained.pdparams
 python tools/export_model.py -c ppcls/configs/ImageNet/GhostNet/GhostNet_x1_0.yaml -o Global.pretrained_model=./pretrained/GhostNet_x1_0_pretrained -o Global.save_inference_dir=./inference_model/GhostNet_x1_0
 
 # DenseNet121
-wget --no-proxy -P pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/DenseNet121_pretrained.pdparams
+wget --no-proxy -P -q pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/DenseNet121_pretrained.pdparams
 python tools/export_model.py -c ppcls/configs/ImageNet/DenseNet/DenseNet121.yaml -o Global.pretrained_model=./pretrained/DenseNet121_pretrained -o Global.save_inference_dir=./inference_model/DenseNet121
 
 # HRNet_W32_C
-wget --no-proxy -P pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/legendary_models/HRNet_W32_C_pretrained.pdparams
+wget --no-proxy -P -q pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/legendary_models/HRNet_W32_C_pretrained.pdparams
 python tools/export_model.py -c ppcls/configs/ImageNet/HRNet/HRNet_W32_C.yaml -o Global.pretrained_model=./pretrained/HRNet_W32_C_pretrained -o Global.save_inference_dir=./inference_model/HRNet_W32_C
 
 # InceptionV4
-wget --no-proxy -P pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/InceptionV4_pretrained.pdparams
+wget --no-proxy -P -q pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/InceptionV4_pretrained.pdparams
 python tools/export_model.py -c ppcls/configs/ImageNet/Inception/InceptionV4.yaml -o Global.pretrained_model=./pretrained/InceptionV4_pretrained -o Global.save_inference_dir=./inference_model/InceptionV4
 
 # ViT_base_patch16_224
-wget --no-proxy -P pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/ViT_base_patch16_224_pretrained.pdparams
+wget --no-proxy -P -q pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/ViT_base_patch16_224_pretrained.pdparams
 python tools/export_model.py -c ppcls/configs/ImageNet/VisionTransformer/ViT_base_patch16_224.yaml -o Global.pretrained_model=./pretrained/ViT_base_patch16_224_pretrained -o Global.save_inference_dir=./inference_model/ViT_base_patch16_224
 
 # DeiT_base_patch16_224
-wget --no-proxy -P pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/DeiT_base_patch16_224_pretrained.pdparams
+wget --no-proxy -P -q pretrained/ https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/DeiT_base_patch16_224_pretrained.pdparams
 python tools/export_model.py -c ppcls/configs/ImageNet/DeiT/DeiT_base_patch16_224.yaml -o Global.pretrained_model=./pretrained/DeiT_base_patch16_224_pretrained -o Global.save_inference_dir=./inference_model/DeiT_base_patch16_224
 
 # 复制推理模型
@@ -380,23 +123,25 @@ sh compile.sh clas
 git clone https://github.com/PaddlePaddle/PaddleDetection.git --depth=1
 cd PaddleDetection
 
+python -m pip install -r requirements.txt -i https://mirror.baidu.com/pypi/simple --trusted-host mirror.b+aidu.com
+
 mkdir pretrained
 mkdir inference_model
 
 # faster_rcnn_r50_fpn_1x_coco
-wget --no-proxy -P pretrained/ https://paddledet.bj.bcebos.com/models/faster_rcnn_r50_fpn_1x_coco.pdparams
+wget --no-proxy -P -q pretrained/ https://paddledet.bj.bcebos.com/models/faster_rcnn_r50_fpn_1x_coco.pdparams
 python tools/export_model.py -c configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.yml --output_dir=./inference_model/ -o weights=pretrained/faster_rcnn_r50_fpn_1x_coco.pdparams
 
 # mask_rcnn_r50_vd_fpn_2x_coco
-wget --no-proxy -P pretrained/ https://paddledet.bj.bcebos.com/models/mask_rcnn_r50_vd_fpn_2x_coco.pdparams
+wget --no-proxy -P -q pretrained/ https://paddledet.bj.bcebos.com/models/mask_rcnn_r50_vd_fpn_2x_coco.pdparams
 python tools/export_model.py -c configs/mask_rcnn/mask_rcnn_r50_vd_fpn_2x_coco.yml --output_dir=./inference_model/ -o weights=pretrained/mask_rcnn_r50_vd_fpn_2x_coco.pdparams
 
 # yolov3_darknet53_270e_coco
-wget --no-proxy -P pretrained/ https://paddledet.bj.bcebos.com/models/yolov3_darknet53_270e_coco.pdparams
+wget --no-proxy -P -q pretrained/ https://paddledet.bj.bcebos.com/models/yolov3_darknet53_270e_coco.pdparams
 python tools/export_model.py -c configs/yolov3/yolov3_darknet53_270e_coco.yml --output_dir=./inference_model/ -o weights=pretrained/yolov3_darknet53_270e_coco.pdparams
 
 # ssd_mobilenet_v1_300_120e_voc
-wget --no-proxy -P pretrained/ https://paddledet.bj.bcebos.com/models/ssd_mobilenet_v1_300_120e_voc.pdparams
+wget --no-proxy -P -q pretrained/ https://paddledet.bj.bcebos.com/models/ssd_mobilenet_v1_300_120e_voc.pdparams
 python tools/export_model.py -c configs/ssd/ssd_mobilenet_v1_300_120e_voc.yml --output_dir=./inference_model/ -o weights=pretrained/ssd_mobilenet_v1_300_120e_voc.pdparams
 
 cp -r ./inference_model/* ../inference_model/
@@ -442,19 +187,19 @@ sh compile.sh detect
 # PaddleOCR 模型测试
 mkdir ocr_inference_model && cd ocr_inference_model
 
-wget https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_det_infer.tar
+wget -q https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_det_infer.tar
 tar xf ch_ppocr_mobile_v2.0_det_infer.tar
 
-wget https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_cls_infer.tar
+wget -q https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_cls_infer.tar
 tar xf ch_ppocr_mobile_v2.0_cls_infer.tar
 
-wget https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_rec_infer.tar
+wget -q https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_rec_infer.tar
 tar xf ch_ppocr_mobile_v2.0_rec_infer.tar
 
-wget https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_server_v2.0_det_infer.tar
+wget -q https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_server_v2.0_det_infer.tar
 tar xf ch_ppocr_server_v2.0_det_infer.tar
 
-wget https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_server_v2.0_rec_infer.tar
+wget -q https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_server_v2.0_rec_infer.tar
 tar xf ch_ppocr_server_v2.0_rec_infer.tar
 
 cd -
@@ -494,7 +239,7 @@ sh compile.sh ocr_rec
 
 # PaddleNLP 模型测试
 mkdir nlp_inference_model && cd nlp_inference_model
-wget http://paddle-inference-dist.bj.bcebos.com/tensorrt_test/ernie_model_4.tar.gz
+wget -q http://paddle-inference-dist.bj.bcebos.com/tensorrt_test/ernie_model_4.tar.gz
 tar xzf ernie_model_4.tar.gz
 cd -
 
@@ -514,7 +259,7 @@ cd ../ernie-varlen/ && sed -i "s#TENSORRT_ROOT=/root/work/nvidia/TensorRT-7.2.3.
 sh compile.sh
 
 if [ ! -d ernie_model_4 ]; then
-    wget http://paddle-inference-dist.bj.bcebos.com/tensorrt_test/ernie_model_4.tar.gz;
+    wget -q http://paddle-inference-dist.bj.bcebos.com/tensorrt_test/ernie_model_4.tar.gz;
     tar xzf ernie_model_4.tar.gz;
 fi
 
