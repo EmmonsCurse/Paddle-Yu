d1ddd8b16ffa7c49a1f2ec0f852280941d18ef70
YuBaoku <49938469+EmmonsCurse@users.noreply.github.com>
2022-11-07 17:24:15 +0800

    Update Release_run_PID_ce_test_demo.sh

--- Release_run_PID_ce_test_demo.sh
+++ Release_run_PID_ce_test_demo.sh
@@ -147,11 +147,12 @@ echo "demo 6.4: fp16_resnet50:"
 python infer_resnet.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams --run_mode=trt_fp16
 count_error
 
-echo "demo 6.5: int8_resnet50:"
+echo "demo 6.5: trt_int8_resnet50_generate:"
 # 使用 Trt Int8 运行样例
 python infer_resnet.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams --run_mode=trt_int8
 count_error
 
+echo "demo 6.5: trt_int8_resnet50_generate:"
 python infer_resnet.py --model_file=../../cpu/resnet50/resnet50/inference.pdmodel --params_file=../../cpu/resnet50/resnet50/inference.pdiparams --run_mode=trt_int8
 count_error
 
@@ -172,25 +173,33 @@ if [ ! -f kite.jpg ]; then
     wget -q https://paddle-inference-dist.bj.bcebos.com/inference_demo/images/kite.jpg
 fi
 
+echo "demo 7.1: gpu_yolov3_native:"
 # 使用原生 GPU 运行样例
 python infer_yolov3.py --model_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdiparams
 count_error
 
+echo "demo 7.2: fp32_yolov3:"
 # 使用 Trt Fp32 运行样例
 python infer_yolov3.py --model_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_fp32
 count_error
 
+
+echo "demo 7.3: fp16_yolov3:"
 # 使用 Trt Fp16 运行样例
 python infer_yolov3.py --model_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_fp16
 count_error
 
+
+echo "demo 7.4: trt_int8_yolov3_generate:"
 # 使用 Trt Int8 运行样例
 python infer_yolov3.py --model_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_int8
 count_error
 
+echo "demo 7.4: trt_int8_yolov3:"
 python infer_yolov3.py --model_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_int8
 count_error
 
+echo "demo 7.4: trt_dynamic_shape_yolov3:"
 # 使用 Try dynamic shape 运行样例 -error
 python infer_yolov3.py --model_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file=../../cpu/yolov3/yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_fp32 --use_dynamic_shape=1
 count_error
@@ -245,7 +254,7 @@ python cam_video.py
 count_error
 
 
-
+echo "demo 9: ELMo_LAC:"
 # demo 11: LSTM INT8 prediction example on X86 Linux
 demo=`expr ${demo} + 1`
 
@@ -278,11 +287,13 @@ sed -i "s/python3/python/g" run.sh
 bash run.sh
 count_error
 
+echo "demo 9.1: ELMo_LAC_thread:"
 # 1 thread
 # run fp32 model
 python model_test.py --model_path=./lstm_fp32_model --data_path=./quant_lstm_input_data --use_analysis=False --num_threads=1
 count_error
 
+echo "demo 9.1: ELMo_LAC_4_threads:"
 # 4 thread
 # run fp32 model
 python model_test.py --model_path=./lstm_fp32_model --data_path=./quant_lstm_input_data --use_analysis=False --num_threads=4
@@ -298,6 +309,7 @@ cd ../../../c++/lib/
 wget -q https://paddle-qa.bj.bcebos.com/paddle-pipeline/Release-GpuAll-Centos-Gcc82-Cuda112-Cudnn82-Trt8034-Py38-Compile/latest/paddle_inference.tgz
 tar xzf paddle_inference.tgz
 
+echo "demo 1: cpu-resnet50:"
 # demo 1 cpu-resnet50
 demo=`expr ${demo} + 1`
 
@@ -306,51 +318,63 @@ cd ../cpu/resnet50/
 sed -i "s/WITH_ONNXRUNTIME=ON/WITH_ONNXRUNTIME=OFF/" compile.sh
 
 # 使用 oneDNN 运行样例
+echo "demo 1.1: cpu_resnet50_mkldnn:"
 sh run.sh
 count_error
 
+echo "demo 1.2: cpu_resnet50_onnxruntime:"
 # 使用 OnnxRuntime 运行样例
 ./build/resnet50_test --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --use_ort=1
 count_error
 
 
+echo "demo 2: cpu-yolov3:"
 # demo 2 cpu-yolov3
 demo=`expr ${demo} + 1`
 
 # 使用 oneDNN 运行样例
+echo "demo 2.1: cpu_yolov3_mkldnn:"
 cd ../yolov3/ && sh run.sh
 count_error
 
+echo "demo 2.2: cpu_yolov3_onnxruntime:"
 # 使用 OnnxRuntime 运行样例 -[ERROR] Cannot found attribute iou_aware in op: yolo_box
 ./build/yolov3_test --model_file yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file yolov3_r50vd_dcn_270e_coco/model.pdiparams --use_ort=1
 count_error
 
 
+echo "demo 3: gpu-resnet50:"
 # demo 3 gpu-resnet50
 demo=`expr ${demo} + 1`
 
+echo "demo 3.1: gpu_resnet50_native:"
 # 使用原生 GPU 运行样例
 cd ../../gpu/resnet50/
 sed -i "s/TensorRT-7.1.3.4/TensorRT-8.0.3.4/" compile.sh && sh run.sh
 count_error
 
+echo "demo 3.2: fp32_resnet50:"
 # 使用 TensorRT Fp32 运行样例
 ./build/resnet50_test --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --run_mode=trt_fp32
 count_error
 
+echo "demo 3.3: fp16_resnet50:"
 # 使用 TensorRT Fp16 运行样例
 ./build/resnet50_test --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --run_mode=trt_fp16
 count_error
 
+echo "demo 3.4: trt_int8_resnet50_generate:"
 # 使用 TensorRT Int8 离线量化预测运行样例
 # 生成量化校准表
 ./build/resnet50_test --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --run_mode=trt_int8
 count_error
 
+echo "demo 3.5: trt_int8_resnet50:"
 # 加载校准表执行预测
 ./build/resnet50_test --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --run_mode=trt_int8 --use_calib=true
 count_error
 
+echo "demo 3.6: Slim_trt_int8_resnet50_quant:"
 # 使用 TensorRT 加载 PaddleSlim Int8 量化模型预测
 # 模型下载 
 wget -q https://paddle-inference-dist.bj.bcebos.com/inference_demo/python/resnet50/ResNet50_quant.tar.gz
@@ -359,40 +383,49 @@ tar -zxvf ResNet50_quant.tar.gz
 ./build/resnet50_test --model_dir ResNet50_quant/ --run_mode=trt_int8 --use_calib=false
 count_error
 
+echo "demo 3.7: trt_idynamic_shape_resnet50:"
 # 使用 TensorRT dynamic shape 运行样例（以 Fp32 为例）-error
 ./build/resnet50_test --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --run_mode=trt_fp32 --use_dynamic_shape=1
 count_error
 
 
+echo "demo 4: gpu-yolov3:"
 # demo 4 gpu-yolov3
 demo=`expr ${demo} + 1`
 
+echo "demo 4.1: gpu_yolov3_native:"
 # 使用原生 GPU 运行样例
 cd ../yolov3/ && sed -i "s/TensorRT-7.1.3.4/TensorRT-8.0.3.4/" compile.sh && sh run.sh
 count_error
 
+echo "demo 4.2: fp32_yolov3:"
 # 使用 TensorRT Fp32 运行样例
 ./build/yolov3_test --model_file yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_fp32
 count_error
 
+echo "demo 4.3: fp16_yolov3:"
 # 使用 TensorRT Fp16 运行样例
 ./build/yolov3_test --model_file yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_fp16
 count_error
 
+echo "demo 4.4: trt_int8_yolov3_generate:"
 # 使用 TensorRT Int8 运行样例
 # 生成量化校准表
 ./build/yolov3_test --model_file yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_int8
 count_error
 
+echo "demo 4.5: trt_int8_yolov3:"
 # 加载校准表执行预测
 ./build/yolov3_test --model_file yolov3_r50vd_dcn_270e_coco/model.pdmodel --params_file yolov3_r50vd_dcn_270e_coco/model.pdiparams --run_mode=trt_int8
 count_error
 
+echo "demo 4.6: trt_dynamic_shape_yolov3:"
 # 使用 TensorRT dynamic shape 运行样例（以 Fp32 为例）-error
 ./build/yolov3_test --model_file yolov3_r50vd_dcn_270e_coco/inference.pdmodel --params_file yolov3_r50vd_dcn_270e_coco/inference.pdiparams --run_mode=trt_fp32 --use_dynamic_shape=1
 count_error
 
 
+echo "demo 5: gpu-tuned_dynamic_shape"
 # demo 5 gpu-tuned_dynamic_shape
 demo=`expr ${demo} + 1`
 
@@ -448,6 +481,7 @@ sh compile.sh clas
 
 # 注意以下 CV 类的测试 case，不是真正的变长模型，对输入 shape 是有要求的，无法支持任意的输入 shape
 
+echo "demo 5.1: gpu-tuned_dynamic_shape_ResNet50_Vd"
 # ResNet50_Vd
 # 离线tune测试
 ./build/clas --model_file inference_model/ResNet50_vd/inference.pdmodel  --params_file inference_model/ResNet50_vd/inference.pdiparams --hs="224:448" --ws="224:448" --tune
@@ -462,6 +496,7 @@ count_error
 count_error
 
 
+echo "demo 5.2: gpu-tuned_dynamic_shape_GhostNet_x1_0"
 # GhostNet_x1_0
 # 离线tune测试
 ./build/clas --model_file inference_model/GhostNet_x1_0/inference.pdmodel --params_file inference_model/GhostNet_x1_0/inference.pdiparams --hs="224:448" --ws="224:448" --tune
@@ -476,6 +511,7 @@ count_error
 count_error
 
 
+echo "demo 5.3: gpu-tuned_dynamic_shape_DenseNet121"
 # DenseNet121
 # 离线tune测试
 ./build/clas --model_file inference_model/DenseNet121/inference.pdmodel --params_file inference_model/DenseNet121/inference.pdiparams --hs="224:448" --ws="224:448" --tune
@@ -490,6 +526,7 @@ count_error
 count_error
 
 
+echo "demo 5.4: gpu-tuned_dynamic_shape_HRNet_W32_C"
 # HRNet_W32_C
 # 离线tune测试
 ./build/clas --model_file inference_model/HRNet_W32_C/inference.pdmodel --params_file inference_model/HRNet_W32_C/inference.pdiparams --hs="224" --ws="224" --tune
@@ -504,6 +541,7 @@ count_error
 count_error
 
 
+echo "demo 5.5: gpu-tuned_dynamic_shape_InceptionV4"
 # InceptionV4
 # 离线tune测试
 ./build/clas --model_file inference_model/InceptionV4/inference.pdmodel --params_file inference_model/InceptionV4/inference.pdiparams --hs="224" --ws="224" --tune
@@ -518,6 +556,7 @@ count_error
 count_error
 
 
+echo "demo 5.6: gpu-tuned_dynamic_shape_ViT_base_patch16_224"
 # ViT_base_patch16_224
 
 # config 加配置
@@ -537,6 +576,7 @@ count_error
 count_error
 
 
+echo "demo 5.6: gpu-tuned_dynamic_shape_DeiT_base_patch16_224"
 # DeiT_base_patch16_224
 # 离线tune测试
 ./build/clas --model_file inference_model/DeiT_base_patch16_224/inference.pdmodel --params_file inference_model/DeiT_base_patch16_224/inference.pdiparams --hs="224" --ws="224" --tune
@@ -585,6 +625,7 @@ cd -
 
 sh compile.sh detect
 
+echo "demo 5.7: gpu-tuned_dynamic_shape_yolov3_darknet53_270e_coco"
 # yolov3_darknet53_270e_coco
 # 离线tune测试
 ./build/detect --model_file inference_model/yolov3_darknet53_270e_coco/model.pdmodel --params_file inference_model/yolov3_darknet53_270e_coco/model.pdiparams --hs="608" --ws="608" --tune
@@ -602,6 +643,7 @@ count_error
 sed -i "75s#// config#config#g" detect.cc
 sh compile.sh detect
 
+echo "demo 5.8: gpu-tuned_dynamic_shape_ssd_mobilenet_v1_300_120e_voc"
 # ssd_mobilenet_v1_300_120e_voc
 # 离线tune测试
 ./build/detect --model_file inference_model/ssd_mobilenet_v1_300_120e_voc//model.pdmodel --params_file inference_model/ssd_mobilenet_v1_300_120e_voc//model.pdiparams --hs="300" --ws="300" --tune
@@ -618,6 +660,7 @@ count_error
 # 修改
 # sed -i "s#"roi_align"#"roi_align\",\"elementwise_add"#" detect.cc
 
+echo "demo 5.9: gpu-tuned_dynamic_shape_faster_rcnn_r50_fpn_1x_coco"
 # faster_rcnn_r50_fpn_1x_coco
 # 离线tune测试
 ./build/detect --model_file inference_model/faster_rcnn_r50_fpn_1x_coco/model.pdmodel --params_file inference_model/faster_rcnn_r50_fpn_1x_coco/model.pdiparams --hs="608" --ws="608" --tune
@@ -632,6 +675,7 @@ count_error
 count_error
 
 
+echo "demo 5.9: gpu-tuned_dynamic_shape_mask_rcnn_r50_vd_fpn_2x_coco"
 # mask_rcnn_r50_vd_fpn_2x_coco
 # 离线tune测试
 ./build/detect --model_file inference_model/mask_rcnn_r50_vd_fpn_2x_coco/model.pdmodel --params_file inference_model/mask_rcnn_r50_vd_fpn_2x_coco/model.pdiparams --hs="608" --ws="608" --tune
@@ -644,6 +688,7 @@ count_error
 # 动态shape及反序列化测试（注意：目前需要config加配置：config->Exp_DisableTensorRtOPs({"roi_align", "elementwise_add"});）
 ./build/detect --model_file inference_model/mask_rcnn_r50_vd_fpn_2x_coco/model.pdmodel --params_file inference_model/mask_rcnn_r50_vd_fpn_2x_coco/model.pdiparams --hs="608" --ws="608" --no_seen_hs="416" --no_seen_ws="416" --tuned_dynamic_shape --serialize
 count_error
+
 
 # PaddleOCR 模型测试
 mkdir ocr_inference_model && cd ocr_inference_model
@@ -668,6 +713,7 @@ cd -
 # ocr det模型测试
 sh compile.sh ocr_det
 
+echo "demo 5.10: gpu-tuned_dynamic_shape_ch_ppocr_server_v2.0_det_infer"
 # 离线tune测试
 ./build/ocr_det --model_file ocr_inference_model/ch_ppocr_server_v2.0_det_infer/inference.pdmodel --params_file ocr_inference_model/ch_ppocr_server_v2.0_det_infer/inference.pdiparams --hs="640" --ws="640" --tune
 count_error
@@ -683,6 +729,7 @@ count_error
 # ocr cls模型测试
 sh compile.sh ocr_cls
 
+echo "demo 5.11: gpu-tuned_dynamic_shape_ch_ppocr_mobile_v2.0_cls_infer"
 # 离线tune测试
 ./build/ocr_cls --model_file ocr_inference_model/ch_ppocr_mobile_v2.0_cls_infer/inference.pdmodel --params_file ocr_inference_model/ch_ppocr_mobile_v2.0_cls_infer/inference.pdiparams --hs="640" --ws="640" --tune
 count_error
@@ -699,6 +746,7 @@ count_error
 # ocr rec模型测试
 sh compile.sh ocr_rec
 
+echo "demo 5.12: gpu-tuned_dynamic_shape_ch_ppocr_mobile_v2.0_rec_infer"
 # 离线tune测试
 ./build/ocr_rec --model_file ocr_inference_model/ch_ppocr_server_v2.0_rec_infer/inference.pdmodel --params_file ocr_inference_model/ch_ppocr_server_v2.0_rec_infer/inference.pdiparams --hs="32" --ws="32" --tune
 count_error
@@ -720,6 +768,7 @@ cd -
 
 sh compile.sh ernie
 
+echo "demo 5.13: gpu-tuned_dynamic_shape_ernie_model_4"
 # 离线tune测试
 ./build/ernie --model_dir nlp_inference_model/ernie_model_4 --tune
 count_error
@@ -745,6 +794,7 @@ if [ ! -d ernie_model_4 ]; then
     tar xzf ernie_model_4.tar.gz;
 fi
 
+echo "demo 6: ernie_varlen_test"
 ./build/ernie_varlen_test --model_dir=./ernie_model_4/
 count_error
 
@@ -752,10 +802,13 @@ count_error
 # demo 7 gpu-gpu_fp16
 demo=`expr ${demo} + 1`
 
+
+echo "demo 7.1: gpu_resnet_50_native"
 # 使用原生 GPU 运行样例
 cd ../gpu_fp16 && sed -i "s/TensorRT-7.1.3.4/TensorRT-8.0.3.4/" compile.sh && sh run.sh
 count_error
 
+echo "demo 7.2: gpu_resnet_50_mix"
 # 使用 GPU 混合精度推理 运行样例
 ./build/resnet50_gpu_fp16 --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --use_gpu_fp16=1
 count_error
@@ -763,6 +816,8 @@ count_error
 # demo 8 gpu-multi_stream
 demo=`expr ${demo} + 1`
 
+
+echo "demo 8: gpu_resnet_50_mix"
 # 运行 GPU 多流预测样例 
 # 该功能 develop 和 2.4 后可用
 cd ../multi_stream && sed -i "s/TensorRT-7.1.3.4/TensorRT-8.0.3.4/" compile.sh && sh run.sh
@@ -772,25 +827,30 @@ count_error
 cd ../experimental/batch_stream/ && sed -i "s/TensorRT-7.1.3.4/TensorRT-8.0.3.4/" compile.sh && sh run.sh
 count_error
 
+echo "demo 9: advanced-custom_operator"
 # demo 9 advanced-custom_operator
 # 自定义算子模型构建运行示例
 cd ../../../advanced/custom-operator/ && sed -i "s/WITH_ONNXRUNTIME=ON/WITH_ONNXRUNTIME=OFF/" compile.sh && sed -i "s/TensorRT-7.1.3.4/TensorRT-8.0.3.4/" compile.sh && sh run.sh
 count_error
 
 
+echo "demo 10: advanced-share_external_data"
 # demo 10 advanced-share_external_data
 demo=`expr ${demo} + 1`
 
 # 运行 share_external_data 预测样例
 # 运行 CPU 样例
+echo "demo 10.1: advanced-share_external_data_cpu"
 cd ../share_external_data/ && sed -i "s/TensorRT-7.1.3.4/TensorRT-8.0.3.4/" compile.sh && sh run.sh
 count_error
 
+echo "demo 10.2: advanced-share_external_data_gpu"
 # 运行 GPU 样例
 ./build/resnet50_share_data --model_file resnet50/inference.pdmodel --params_file resnet50/inference.pdiparams --use_gpu=1
 count_error
 
 
+echo "demo 11: advanced-multi_thread"
 # demo 11 advanced-multi_thread
 demo=`expr ${demo} + 1`
 
@@ -799,11 +859,13 @@ cd ../share_external_data/ && sed -i "s/TensorRT-7.1.3.4/TensorRT-8.0.3.4/" comp
 count_error
 
 
+echo "demo 12: advanced x86_gru_int8"
 # demo 12 advanced x86_gru_int8
 # 需要在 manylinux_cpu_avx_mkl_gcc82 预测库下
 demo=`expr ${demo} + 1`
 
 
+echo "demo 13: mixed-LIC2020"
 # demo 13 mixed-LIC2020
 demo=`expr ${demo} + 1`
 
